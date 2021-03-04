2cb76934f076a582cfdd8f40120088a0207dbf1f
Egor <egormikulic@gmail.com>
2021-03-04 15:08:23 +0300

    Adding migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialNetwork.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
     }
 }
--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -7,5 +7,8 @@ namespace SocialNetwork.Entities
 {
     public class AppUser
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+       
     }
 }
